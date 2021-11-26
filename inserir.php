eb0a91840d6bb5134c86701bf54d898292debbc1
Djailson1 <djailson_silva1@outlook.com>
2021-11-26 00:08:26 -0300

    banco de dados exercício

---
+++ inserir.php
@@ -0,0 +1,10 @@
+<?php
+
+$aluno= $_POST['aluno'];
+$disciplina= $_POST['disciplina'];
+$nota1= $_POST['nota1'];
+$nota2= $_POST['nota2'];
+$nota3= $_POST['nota3'];
+
+echo("Aluno: $aluno\n\nDisciplina: $disciplina\n\nNota 1: $nota1\n\nNota 2: $nota2\n\nNota 3: $nota3");
+?>
